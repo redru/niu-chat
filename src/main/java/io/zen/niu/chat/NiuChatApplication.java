e24ee73bb3d8b67d9fa92b0c4c946b9a5ed25a1d
Luca <luzenobi@gmail.com>
2021-03-05 22:59:30 +0100

    First commit

---
+++ src/main/java/io/zen/niu/chat/NiuChatApplication.java
@@ -0,0 +1,13 @@
+package io.zen.niu.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiuChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiuChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/zen/niu/chat/NiuChatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zen.niu.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiuChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
